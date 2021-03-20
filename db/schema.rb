96e01e6c7d8b4b5ffa9e58f4838206a31b397539
y.watanabe <trance3912hkss@gmail.com>
2021-03-20 22:37:29 +0900

    一覧表示機能 を実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_20_123227) do
+ActiveRecord::Schema.define(version: 2021_03_20_132326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "title"
+    t.string "contents"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
