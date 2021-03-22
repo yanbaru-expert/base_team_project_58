f36998ecc0802ffd6a7fadb5cfab46adcec0ac87
阿部豊 <k1819abe@gmail.com>
2021-03-22 22:24:58 +0900

    新規投稿機能 new, create を実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,15 @@ class MessagesController < ApplicationController
   def new
     @message = Message.new
   end
+
+  def create
+    # Strong Parameter の記述
+    Message.create(message_params)
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:title, :content)
+  end
 end
