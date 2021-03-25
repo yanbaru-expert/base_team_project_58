c7bd71ccf80731a4886fe626f965efea47145e83
yasu-grz <yatthun82@gmail.com>
2021-03-26 06:06:48 +0900

    1.「ユーザー」の「マイグレーションファイル」と「モデル」を作成しマイグレーションを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_20_132326) do
+ActiveRecord::Schema.define(version: 2021_03_24_223007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2021_03_20_132326) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.string "contents"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
