94bfbb4875863b5b610a84892c944c535eb0bf18
yasu-grz <yatthun82@gmail.com>
2021-03-26 07:15:40 +0900

    「ユーザー」新規投稿機能 new,createを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,11 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    User.create(user_params)
   end
 
   def edit
@@ -18,5 +20,10 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :age)
   end
 end
