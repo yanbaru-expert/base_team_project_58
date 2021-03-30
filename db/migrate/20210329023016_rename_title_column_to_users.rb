93265f8029b9a93d2329856ced4e79fab5fac5b1
yasu-grz <yatthun82@gmail.com>
2021-03-30 20:50:07 +0900

    「ユーザー」CRUD処理の修正

--- db/migrate/20210329023016_rename_title_column_to_users.rb
+++
@@ -1,5 +0,0 @@
-class RenameTitleColumnToUsers < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :users, :title, :name
-  end
-end
--- db/migrate/20210329024211_rename_content_column_to_users.rb
+++
@@ -1,5 +0,0 @@
-class RenameContentColumnToUsers < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :users, :content, :age
-  end
-end
